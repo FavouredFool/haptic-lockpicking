306ae70709e362d3cfdb2f300c06c476de6fcfaf
Tim Wahrburg <t.wahrburg@gmail.com>
2023-03-05 19:41:12 +0100

    fix: removed inline parameters (they were so hideous)

--- haptic lockpicking/Assets/Lockpicking/Scripts/Lock/LockManager.cs
+++ haptic lockpicking/Assets/Lockpicking/Scripts/Lock/LockManager.cs
@@ -11,18 +11,6 @@ public class LockManager : MonoBehaviour
     [SerializeField]
     LockBuilder _lockBuilder;
 
-
-    List<int> pinCountParameters = new() { 5, };
-    List<List<int>> pinOrderParameters;
-    List<bool> respectOrderParameters = new() { true };
-    List<bool> hasPickParameters = new() { true };
-    List<bool> hasTensionParameters = new() { true };
-    List<bool> colorCodePinsParameters = new() { true };
-    List<bool> showTensionIndicatorParameters = new() { false };
-    List<bool> showPinPositionIndicatorParameters = new() { false };
-    List<CutoutState> cutoutStateParameters = new() { CutoutState.FULL };
-
-
     public void Awake()
     {
         if (Instance != null && Instance != this)
@@ -33,11 +21,6 @@ public class LockManager : MonoBehaviour
         {
             Instance = this;
         }
-    }
-
-    public void Start()
-    {
-        pinOrderParameters = new() { PinManager.Instance.GetRandomPinOrder(5) };
     }
 
     public void CreateNewLock(TutorialSectionInformation _information)
@@ -53,8 +36,6 @@ public class LockManager : MonoBehaviour
         {
             _information.PinOrder = PinManager.Instance.GetRandomPinOrder(_information.PinCount);
         }
-
-        Debug.Log(_information.PinCount + " " + _information.PinOrder[0] + _information.RespectOrder + _information.HasPick +  _information.HasTension + _information.ColorCodePins + _information.ShowTensionIndicator + _information.ShowPinPositionIndicator + _information.CutoutState);
 
         _lockBuilder.SetParameters(_information.PinCount, _information.PinOrder, _information.RespectOrder, _information.HasPick, _information.HasTension, _information.ColorCodePins, _information.ShowTensionIndicator, _information.ShowPinPositionIndicator, _information.CutoutState);
 
