afb75cc6f0279764dda6e77221a90bf93a3e0a40
Tim Wahrburg <t.wahrburg@gmail.com>
2023-04-04 00:49:03 +0200

    fix: buttons auf deutsch umgeschrieben

--- haptic lockpicking/Assets/Lockpicking/Scripts/Tutorial/TutorialUI.cs
+++ haptic lockpicking/Assets/Lockpicking/Scripts/Tutorial/TutorialUI.cs
@@ -19,7 +19,7 @@ public class TutorialUI : MonoBehaviour
 
     public void SetSectionLabel(int nr, string label)
     {
-        _sectionLabel.text = "Section " + (nr+1) + "/12: " + label;
+        _sectionLabel.text = "Sektion " + (nr+1) + "/12: " + label;
     }
 
     public void ClearTexts()
